11f754e959e2501637c2ac3989d2af6a09041668
Jonathan Sloan <jsloan117@gmail.com>
2017-05-13 13:53:18 -0400

    Update Transmission_Manager.sh

--- Transmission_Manager.sh
+++ Transmission_Manager.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 #======================================================================================================================================================================================================
 # Name:                 transmission_manager.sh
-# By:                   Jonathan M. Sloan <jsloan@macksarchive.com>
-# Date:                 11-01-2015
+# Date:                 05-13-2017
 # Purpose:              Used to manage torrents in transmission
-# Version:              1.1
 #=====================================================================================================================================================================================================
 
 # Path to transmission-remote
-TR="/usr/bin/transmission-remote"
+TR="/usr/bin/transmission-remote HOST:PORT"
 
 remove_finished_torrents () { # Remove finished torrents
-local LIST=$($TR -l | tail -n+2 | grep Finished | awk '{ print $1 }')
-[[ -z $LIST ]] && echo -e "\nThere are currently no torrents listed in Transmission\n" && exit
+local TOR_LIST=$($TR -l | tail -n+2 | grep Finished | awk '{ print $1 }')
+[[ -z $TOR_LIST ]] && echo -e "\nThere are currently no torrents listed in Transmission\n" && exit 5
 
-for ID in $LIST; do
+for ID in $TOR_LIST; do
 
   local NAME="$($TR -t $ID -i | grep Name:)"
   echo "$ID: ${NAME#*Name: }"
 
-  $TR -t $ID -r > /dev/null 2>&1
+  $TR -t $ID -r
+
+done
+}
+
+delete_torrent () { # Delete torrent AND its data!!!
+[[ -z "$1" ]] && echo -e "\nYou must supply at least one torrent id to completely delete it and its data from transmission/drive.\n" && exit 5
+
+for torrentid in "$@"; do
+
+  $TR -t "$torrentid" --remove-and-delete
+
+done
+}
+
+cleanup_dead_torrents () {
+local TOR_LIST=$($TR -l | grep "Idle" | egrep -w "0\%|n\/a" | awk '{ print $1 }')
+[[ -z $TOR_LIST ]] && echo -e "\nThere are currently no dead/inactive torrents in Transmission\n" && exit 5
+
+for ID in $TOR_LIST; do
+
+  local NAME="$($TR -t $ID -i | grep Name:)"
+  echo "$ID: ${NAME#*Name: }"
+
+  $TR -t $ID --remove-and-delete
 
 done
 }
@@ -30,7 +52,7 @@ local ACTIVE_NUM=$($TR -l | tail -n+2 | grep -vE "100%|Stopped|Idle" | grep -v "
 local ACTIVE=$($TR -l | tail -n+2 | grep -vE "100%|Stopped|Idle" | grep -v "^Sum\:" | sort -nrk2)
 local SUM=$($TR -l | tail -n+2 | grep -vE "100%|Stopped|Idle" | grep -w "^Sum\:")
 local IDLE_NUM=$($TR -l | tail -n+2 | grep "Idle" | grep -v "^Sum\:" | wc -l)
-local IDLE=$($TR -l | tail -n+2 | grep "Idle" | grep -v "^Sum\:")
+local IDLE=$($TR -l | tail -n+2 | grep "Idle" | grep -v "^Sum\:" | sort -nrk2)
 
 printf "There is %d active torrent currently within Transmission\n\n" "$ACTIVE_NUM"
 printf "%s\n\n" "$HEADER"
@@ -60,10 +82,10 @@ done
 }
 
 list_torrents () {
-local LIST=$($TR -l | tail -n+2 | grep -v "^Sum\:" | awk '{ print $1 }')
-[[ -z $LIST ]] && echo -e "\nThere are currently no torrents listed in Transmission\n" && exit
+local TOR_LIST=$($TR -l | tail -n+2 | grep -v "^Sum\:" | awk '{ print $1 }')
+[[ -z $TOR_LIST ]] && echo -e "\nThere are currently no torrents listed in Transmission\n" && exit 5
 
-for ID in $LIST; do
+for ID in $TOR_LIST; do
 
   local NAME="$($TR -t $ID -i | grep Name:)"
   echo -e "$ID: ${NAME#*Name: }"
@@ -73,26 +95,26 @@ done
 
 verify_torrents () { # Verify torrents
 read -ep "Please enter torrent ID number: " TOR_ID
-[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit
-$TR -t $TOR_ID -v
+[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit 5
+echo -e "\n"; $TR -t $TOR_ID -v
 }
 
 torrent_information () { # Check torrent information
 read -ep "Please enter torrent ID number: " TOR_ID
-[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit
-$TR -t $TOR_ID -i
+[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit 5
+echo -e "\n"; $TR -t $TOR_ID -i
 }
 
 list_torrent_files () { # torrents file list
 read -ep "Please enter torrent ID number: " TOR_ID
-[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit
-$TR -t $TOR_ID -f
+[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit 5
+echo -e "\n"; $TR -t $TOR_ID -f
 }
 
 ask_for_more_peers () {
 read -ep "Please enter torrent ID number: " TOR_ID
-[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit
-$TR -t $TOR_ID --reannounce
+[[ -z "$TOR_ID" ]] && echo -e "\nYou must supply a torrent ID\n" && exit 5
+echo -e "\n"; $TR -t $TOR_ID --reannounce
 }
 
 session_info () {
@@ -104,24 +126,32 @@ $TR -st
 }
 
 help_menu () {
-version='1.1'
 prog="$(echo $(basename $0))"
 
 cat <<EOF
-This script is used to manage transmission daemon. You can add/remove/verify torrents, check out session related info, list torrents.
+This script is used to manage torrents within the transmission daemon. You can add/remove/verify torrents, check out session related info, list torrents, list files, ask for more peers.
+Becareful using the -c|--cleanuptorrents as this is automated, and will grep for Idleing torrents with a progress/status of 0% and n/a.
 
-  $prog <[-r|--remove] [-a|--active] [-f|--finished] [-n|--new] [-l|--list] [-v|--verify] [-i|--info] [-lf|--files] [-mp|--morepeers] -si [-ss|--stats]>
-  version: $version
+  $prog <[-h|--help] [-rf|--remove-finished] [-d|--delete] [-c|--cleanuptorrents] [-a|--active] [-f|--finished] [-n|--new] [-l|--list] [-v|--verify] [-i|--info] [-lf|--files] [-mp|--morepeers] [-si] [-ss|--stats]>
 
 EOF
 }
 
-var="$1"
-case $var in
+selection=$1
+case $selection in
 
-  -r|--remove)
+  -rf|--remove-finished)
 
     remove_finished_torrents ;;
+
+  -d|--delete)
+
+    shift
+    delete_torrent "$@" ;;
+
+  -c|--cleanuptorrents)
+
+    cleanup_dead_torrents ;;
 
   -a|--active)
 
