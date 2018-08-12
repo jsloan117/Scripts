1db1351446491399bed68cac060b22942f78116d
Jonathan Sloan <jsloan117@gmail.com>
2018-08-12 09:45:53 -0500

    Update mediasvr_software_upgrader.sh
    added nzbhydra and updated sabnzbd function.

--- mediasvr_software_upgrader.sh
+++ mediasvr_software_upgrader.sh
@@ -1,131 +1,170 @@
 #!/bin/bash
-# used to upgrade SickRage/CouchPotato/SABnzbd
-# Version: 1.3
+# used to upgrade SickRage/CouchPotato/SABnzbd/NZBHydra Centos 7+
 
-sb_path='/opt/sickbeard'
-sb_base=$(dirname $sb_path)
-sb_user='sickbeard'
-cp_path='/opt/couchpotato'
-cp_base=$(dirname $cp_path)
-cp_user='couchpotato'
-sn_path='/opt/sabnzbd'
-sn_base=$(dirname $sn_path)
-sn_user='sabnzbd'
+cp_path='/data/couchpotato'
+cp_base="$(dirname $cp_path)"
+cp_user='transmission'
+sr_path='/data/sickbeard'
+sr_base="$(dirname $sr_path)"
+sr_user="$cp_user"
+nh_path='/data/nzbhydra'
+nh_base="$(dirname $nh_path)"
+nh_user="$cp_user"
+sb_path='/data/sabnzbd'
+sb_base="$(dirname $sb_path)"
+sb_user="$cp_user"
 backup_prefix='_old'
 
+usage () {
+cat <<EOF
+You can call the script with a single argument of the program name or use the '-a' switch for automated/cron use.
+
+Ex: $0 [sickbeard|couchpotato|sabnzbd|nzbhydra|-a]
+
+EOF
+}
+
 check_directory_exist () {
-x=1
+x=0
 
 if [[ $prog = sickbeard ]]; then
 
-  dir=$sb_path$backup_prefix
+  dir="$sr_path$backup_prefix"
 
 elif [[ $prog = couchpotato ]]; then
 
-  dir=$cp_path$backup_prefix
+  dir="$cp_path$backup_prefix"
 
 elif [[ $prog = sabnzbd ]]; then
 
-  dir=$sn_path$backup_prefix
+  dir="$sb_path$backup_prefix"
+
+elif [[ $prog = nzbhydra ]]; then
+
+  dir="$nh_path$backup_prefix"
 
 fi
 
 while [[ -d $dir ]]; do
 
-    x=$(( $x + 1 ))
+    x=$(( x + 1 ))
     dir=$dir$x
-    [[ -d $dir ]] && dir=$(echo $dir | sed 's|[0-9]||g')
+    [[ -d $dir ]] && dir=$(echo $dir | sed 's/[0-9]*//g')
 
 done
 export dir
 }
 
-upgrade_sickbeard () {
-local bdir=$dir
-cd $sb_base
-service sickbeard stop
-mv sickbeard $bdir
-#git clone https://github.com/sickragetv/sickrage.git # Old SR
-git clone https://github.com/SickRage/SickRage.git # New SR
-mv SickRage sickbeard
-#cp -p $bdir/cache.db sickbeard/
-cp -p $bdir/failed.db sickbeard/
-cp -p $bdir/sickbeard.db sickbeard/
-cp -p $bdir/config.ini sickbeard/
-cp -Rp $bdir/cache sickbeard/
-find $sb_path -type f -exec chmod 640 {} \;
-find $sb_path -type d -exec chmod 750 {} \;
-chmod 750 sickbeard/SickBeard.py
-chown -R $sb_user.$sb_user sickbeard/
-service sickbeard start
+check_for_updates () {
+chk_couchpotato () {
+local prog=couchpotato
+local -r cp_upgrade=$(git --git-dir=$cp_path/.git status -u no | grep -q behind; echo $?)
+
+if [[ $cp_upgrade = 0 ]]; then
+
+  check_directory_exist && upgrade_couchpotato
+
+fi
+}
+
+chk_sickrage () {
+local prog=sickbeard
+local -r sr_upgrade=$(git --git-dir=$sr_path/.git status -u no | grep -q behind; echo $?)
+
+if [[ $sr_upgrade = 0 ]]; then
+
+  check_directory_exist && upgrade_sickrage
+
+fi
+}
+
+chk_nzbhydra () {
+local prog=nzbhydra
+local -r nh_upgrade=$(git --git-dir=$nh_path/.git status -u no | grep -q behind; echo $?)
+
+if [[ $nh_upgrade = 0 ]]; then
+
+  check_directory_exist && upgrade_nzbhydra
+
+fi
+}
+
+chk_sabnzbd () {
+local prog=sabnzbd
+
+if [[ $prog = 'sabnzbd' ]]; then
+
+  check_directory_exist && upgrade_sabnzbd
+
+fi
+}
+
+chk_couchpotato
+chk_sickrage
+chk_nzbhydra
+chk_sabnzbd
 }
 
 upgrade_couchpotato () {
-local bdir=$dir
-cd $cp_base
-service couchpotato stop
-mv couchpotato $bdir
-git clone https://github.com/ruudburger/couchpotatoserver.git
-mv couchpotatoserver couchpotato
-cp -p $bdir/settings.conf couchpotato/
+cd "$cp_base" || exit
+systemctl stop couchpotato
+mv $cp_path $dir
+git clone -q https://github.com/ruudburger/couchpotatoserver.git
+mv couchpotatoserver $cp_path
+cp -p $dir/settings.conf $cp_path
 find $cp_path -type f -exec chmod 640 {} \;
 find $cp_path -type d -exec chmod 750 {} \;
-chmod 750 couchpotato/CouchPotato.py
-chown -R $cp_user.$cp_user couchpotato/
-service couchpotato start
+chmod 750 $cp_path/CouchPotato.py
+chown -R $cp_user.$cp_user $cp_path
+systemctl start couchpotato
+}
+
+upgrade_sickrage () {
+cd "$sr_base" || exit
+systemctl stop sickbeard
+mv $sr_path $dir
+git clone -q https://github.com/SickRage/SickRage.git
+mv SickRage $sr_path
+cp -p $dir/{failed.db,sickbeard.db,config.ini} $sr_path
+cp -Rp $dir/cache $sr_path
+find $sr_path -type f -exec chmod 640 {} \;
+find $sr_path -type d -exec chmod 750 {} \;
+chmod 750 $sr_path/SickBeard.py
+chown -R $sr_user.$sr_user $sr_path
+systemctl start sickbeard
+}
+
+upgrade_nzbhydra () {
+cd "$nh_base" || exit
+systemctl stop nzbhydra
+mv $nh_path $dir
+git clone -q https://github.com/theotherp/nzbhydra.git
+cp -p $dir/{config.cfg,nzbhydra.db} $nh_path
+find $nh_path -type f -exec chmod 640 {} \;
+find $nh_path -type d -exec chmod 750 {} \;
+chmod 750 $nh_path/nzbhydra.py
+chown -R $nh_user.$nh_user $nh_path
+systemctl start nzbhydra
 }
 
 upgrade_sabnzbd () {
-# sn_version assumes sabnzbd is still in the 0.7.XX branch
-# sabnzbd_url assumes the same type url pattern. example: http://domain.com/path/to/file/if/there/is/one/filename.tar.gz/download
-
-check_for_new_version () {
-wget -q --spider http://sourceforge.net/projects/sabnzbdplus/files/sabnzbdplus/$sn_new_version/SABnzbd-$sn_new_version-src.tar.gz/download
-}
-
-local sabnzbd_url="$1"
-local sabnzbdurl="${sabnzbd_url%/*}"
-local sabnzbd_file="${sabnzbdurl##*/}"
-local sabnzbd_changelog_file="$sn_path/CHANGELOG.txt"
-local sn_version=$(head -n2 $sabnzbd_changelog_file | tail -n1 | awk '{ print $1 }' | awk -F'.' '{ print $3 }')
-local next_version=$(( $sn_version + 1 ))
-local sn_new_version="0.7.$next_version"
-local bdir=$dir
-
-if [[ -z $sabnzbd_url ]]; then
-
-  if [[ $(check_for_new_version; echo $?) -ne 0 ]]; then
-
-    echo -e "\nWe are not able to automatically upgrade SABnzbd. There is no new version: $sn_new_version detected, if this is a mistake, you should be able to pass the url as the second argument to get it to work. \n" && exit 5
-
-  fi
-
-fi
-
-cd $sn_base
-service sabnzbd stop
-mv sabnzbd $bdir
-
-if [[ -z $sabnzbd_url ]]; then
-
-  wget -O SABnzbd-$sn_new_version-src.tar.gz http://sourceforge.net/projects/sabnzbdplus/files/sabnzbdplus/$sn_new_version/SABnzbd-$sn_new_version-src.tar.gz/download
-  tar -xaf SABnzbd-$sn_new_version-src.tar.gz
-
-else
-
-  wget -O $sabnzbd_file $sabnzbd_url
-  tar -xaf $sabnzbd_file
-  rm -f $sabnzbd_file
-
-fi
-
-mv SABnzbd* sabnzbd
-cp -p $bdir/config.ini sabnzbd/
-find $sn_path -type f -exec chmod 640 {} \;
-find $sn_path -type d -exec chmod 750 {} \;
-chmod 750 sabnzbd/SABnzbd.py
-chown -R $sn_user.$sn_user sabnzbd/
-service sabnzbd start
+cd "$sb_base" || exit
+systemctl stop sabnzbd
+mv $sb_path $dir
+local saburl=$(wget -q https://github.com/sabnzbd/sabnzbd/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}')
+local sabver=$(echo $saburl | awk -F' ' '{ print $4 }')
+wget -q https://github.com/sabnzbd/sabnzbd/releases/download/$sabver/SABnzbd-$sabver-src.tar.gz
+tar -xaf SABnzbd-$sabver-src.tar.gz
+mv SABnzbd-$sabver $sb_path
+rm -f SABnzbd-$sabver-src.tar.gz
+cp -p $dir/config.ini $sb_path
+cp -Rp $dir/admin $sb_path
+[[ -d "$dir/backupnzbs" ]] && mv $dir/backupnzbs $sb_path
+find $sb_path -type f -exec chmod 640 {} \;
+find $sb_path -type d -exec chmod 750 {} \;
+chmod 750 $sb_path/SABnzbd.py
+chown -R $sb_user.$sb_user $sb_path
+systemctl start sabnzbd
 }
 
 prog="$1"
@@ -133,19 +172,26 @@ case "$prog" in
 
     sickbeard)
 
-        check_directory_exist && upgrade_sickbeard ;;
+        check_directory_exist && upgrade_sickrage ;;
 
     couchpotato)
 
         check_directory_exist && upgrade_couchpotato ;;
 
+    nzbhydra)
+
+        check_directory_exist && upgrade_nzbhydra ;;
+
     sabnzbd)
 
-        shift
-        check_directory_exist && upgrade_sabnzbd "$1" ;;
+        check_directory_exist && upgrade_sabnzbd ;;
+
+    -a)
+
+        check_for_updates ;;
 
     *)
 
-        echo -e "\nEx: $0 [sickbeard|couchpotato|sabnzbd] \n" && exit 1 ;;
+        usage && exit 1 ;;
 
 esac
